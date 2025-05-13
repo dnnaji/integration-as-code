836140356b73baa96d1004b052d5504f098d7c9f
Der <dnnaji@gmail.com>
2025-05-13 17:19:46 -0500

    Add /hello/{name} and /hello-world endpoints in .NET API

--- hello-worlds/dotnet/Program.cs
+++ hello-worlds/dotnet/Program.cs
@@ -7,6 +7,19 @@ app.MapGet("/hello", () => {
     return new { Message = "Hello World from .NET" };
 });
 
+// Define GET endpoint for /hello that takes a name parameter
+app.MapGet("/hello/{name}", (string name) => {
+    // Name should be capitalized
+    name = char.ToUpper(name[0]) + name.Substring(1);
+    return new { Message = $"Hello {name} from .NET" };
+});
+
+// Define GET endpoint for /hello-world
+app.MapGet("/hello-world", () => {
+    // Return an anonymous object, ASP.NET Core handles JSON serialization
+    return new { Message = "Hello World from .NET" };
+});
+
 // Optional: Add a root endpoint for basic check
 app.MapGet("/", () => "Hello World Minimal API Root!");
 
